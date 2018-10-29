c7356f7121db29a0c1f0d458f16fe449b734d2ab
Adelaide <adelaide.yoder@gmail.com>
2018-10-29 13:54:34 -0500

    added migration into database after finishing models

--- Ponderer/Ponderer/Data/Migrations/20181029184737_addedModels.Designer.cs
+++ Ponderer/Ponderer/Data/Migrations/20181029184737_addedModels.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Ponderer.Data;
 
 namespace Ponderer.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181029184737_addedModels")]
+    partial class addedModels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
