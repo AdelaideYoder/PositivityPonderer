c459a12989b51ff56b8a27201d1b917c4f091d20
Adelaide <adelaide.yoder@gmail.com>
2018-10-29 10:40:28 -0500

    project created

---
+++ Ponderer/Ponderer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ponderer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
