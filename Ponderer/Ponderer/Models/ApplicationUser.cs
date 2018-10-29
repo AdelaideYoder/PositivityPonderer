f61b13fb509fc0c3543e111a0c26134a2e0cefe9
Adelaide <adelaide.yoder@gmail.com>
2018-10-29 11:12:42 -0500

    added in the rest of the ApplicationUser model

--- Ponderer/Ponderer/Models/ApplicationUser.cs
+++ Ponderer/Ponderer/Models/ApplicationUser.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,22 @@ namespace Ponderer.Models
 {
     public class ApplicationUser
     {
+        [Key]
+        public int ApplicationUserId { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [NotMapped]
+        public string FullName => $"{FirstName} {LastName}";
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
     }
 }
